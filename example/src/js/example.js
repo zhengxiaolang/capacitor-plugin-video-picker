41ce5d24bbe7d73429b58e6c6544675a831eb41d
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2024-09-15 16:57:32 +0545

    removed example

--- example/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { VideoPicker } from 'capacitor-plugin-video-picker';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    VideoPicker.echo({ value: inputValue })
-}
